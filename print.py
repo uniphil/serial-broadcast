4800146e35ef0eb1b6a31a175ffde17e5e742df1
phil <uniphil@gmail.com>
2019-06-19 11:40:59 -0400

    a bad slow version that seems to mostly work

--- print.py
+++ print.py
@@ -4,36 +4,106 @@ from serial.tools import list_ports
 from PIL import Image
 from time import sleep
 
-SPECIAL_BYTE = 0b10110100
-ROW_LEN_BYTES = 48
-PRINTERS = 6
+PRINTERS = 2
+PRINTER_WIDTH = 384
 
+CMD_PATTERN = 0b11011000
+
+CHUNK_SIZE = 8
+def write_chunked(s, data):
+    for i in range(0, len(data), CHUNK_SIZE):
+        while True:
+            if s.in_waiting > 0:
+                print('chunked: clear buffer', s.in_waiting, s.read(s.in_waiting))
+            # s.reset_input_buffer()
+            s.write(bytearray([CMD_PATTERN | 2]))
+            result = s.read(1)
+            if len(result) == 0:
+                print('oh noooo timeout 2')
+                continue
+            elif ord(result) != 2:
+                print('oh noooo chunk error: {:08b}'.format(ord(result)))
+                continue
+            break
+
+        s.reset_input_buffer()
+        s.write(bytearray(data[i:i + CHUNK_SIZE]))
+        result = s.read(1)
+        if len(result) == 0:
+            print('oh noooo timeout 2 2')
+        elif ord(result) != 3:
+            print('oh noooo chunk end error: {:08b}'.format(ord(result)))
 
 
 def print_im(im, s):
-    width = ROW_LEN_BYTES * 8 * PRINTERS
+    width = PRINTERS * PRINTER_WIDTH
     ratio = im.height / im.width
-    im.resize((width, int(round(width * ratio))), resample=Image.LANCZOS)
-
+    im = im.resize((width, int(round(width * ratio))), resample=Image.LANCZOS)
     im = im.convert('1')
 
+    print('resized to', im.size)
+
     for y in range(im.height):
-        print '# row {}'.format(y)
-        # s.write(SPECIAL_BYTE)
-        # s.write(bytearray([0b00001111] * ROW_LEN_BYTES * PRINTERS))
-        for p in range(PRINTERS):
-            out = bytearray([])
-            for xi in range(p * ROW_LEN_BYTES * 8, (p + 1) * ROW_LEN_BYTES * 8, 8):
-                b = 0
-                for i in range(8):
-                    b <<= 1
-                    px = im.getpixel((xi + i, y)) if xi + i < im.width else 255
-                    black = px < 128
-                    b |= black
-                out.append(b)
-            s.write(out)
-            print ''.join(['{:08b}'.format(x) for x in out])
-            sleep(0.05)
+
+        print('# row {}'.format(y))
+
+        while True:
+            print('clearing buffer...', s.in_waiting)
+            if s.in_waiting > 0:
+                print('row: clear buffer {}'.format(', '.join('{:08b}'.format(b)
+                    for b in s.read(s.in_waiting))))
+            # s.reset_input_buffer()
+            print('cleared.')
+            # sleep(0.1)
+            print('sending command 1... {:8b}'.format(CMD_PATTERN | 1))
+            s.write([CMD_PATTERN | 1])
+            print('waiting for ack 1...')
+            result = s.read(1)
+            if len(result) == 0:
+                print('oh noooo row timeout')
+                sleep(0.3)
+                continue
+            elif ord(result) != 1:
+                print('oh noooo row error: {:08b}'.format(ord(result)))
+                sleep(0.3)
+                continue
+            print('got the right ack!')
+            break
+
+        out = bytearray([])
+        for x in range(PRINTER_WIDTH):
+            b = 0
+            for p in range(PRINTERS):
+                px = im.getpixel((p * PRINTER_WIDTH + x, y))
+                black = px < 128
+                b |= black << p
+            out.append(b)
+        write_chunked(s, out)
+        # sleep(0.1)
+
+
+            # out = bytearray([0] * 8)
+            # x_start = xi * 8;
+            # for pi in range(8):
+
+
+
+
+
+
+        # for p in range(PRINTERS):
+        #     out = bytearray([])
+        #     for xi in range(p * ROW_LEN_BYTES * 8, (p + 1) * ROW_LEN_BYTES * 8, 8):
+        #         b = 0
+        #         for i in range(8):
+        #             b <<= 1
+        #             px = im.getpixel((xi + i, y)) if xi + i < im.width else 255
+        #             black = px < 128
+        #             b |= black
+        #         out.append(b)
+        #     s.write(out)
+        #     print ''.join(['{:08b}'.format(x) for x in out])
+        #     sleep(0.05)
 
 
 if __name__ == '__main__':
@@ -51,8 +121,8 @@ if __name__ == '__main__':
                 '\n'.join(map(lambda m: '{}\t{}\t{}'.format(m.device, m.description, m.manufacturer), maybes))))
             sys.exit(1)
         port = maybes[0].device
-    s = Serial(port, 115200)
-    sleep(0.3)
+    s = Serial(port, 115200, timeout=1.3)
+    sleep(1.3)
     ims = [Image.open(f) for f in filenames]
     while True:
         for im in ims:
--- serial-broadcast/serial-broadcast.ino
+++ serial-broadcast/serial-broadcast.ino
@@ -1,39 +1,95 @@
 #include "ManySoftSerial.h"
 
-ManySoftSerial printers = ManySoftSerial(8, 0b00111111);
+#define ROW_BYTES 48
+
+// states
+#define WAITING 0
+
+// messages
+#define READY 0
+#define BEGIN_ROW 1
+#define PRINT_ROW 2
+
+// asdf
+#define CMD_PATTERN 0b11011000
+
+ManySoftSerial b_printers = ManySoftSerial(8, 0b00111111);
+
+uint8_t state = WAITING;
+
+uint8_t b_buff[8];
+uint8_t b_idx = 0;
+
 
 void setup() {
-  printers.begin(19200);
-
-  uint8_t d[] = {
-    0b00000000,
-    0b00000111,
-    0b00000110,
-    0b00000000,
-    0b00000111,
-    0b00000100,
-    0b00000101,
-    0b00000010};
-
-  for (int i = 0; i < 2; i++) {
-    printers.write8(d);
-    printers.write8(d);
-    printers.write8(d);
-    printers.write8(d);
-    printers.write8(d);
-    printers.write8(d);
-    printers.write8(d);
-    printers.write8(d);
-    printers.write8(d);
-    printers.write8(d);
-    printers.write8(d);
-    printers.write8(d);
-    printers.write8(d);
-  }
-
-  printers.println("\n");
+  Serial.begin(115200);
+  b_printers.begin(19200);
+  b_printers.setTimeout(500);
+  delay(100);
 }
 
 void loop() {
+  while (!Serial.available());
+  byte command = Serial.read();
+  if ((command & 0b11111000) != CMD_PATTERN) {
+    Serial.write(command);
+    return;
+  }
+
+  switch (command & 0b0000111) {
+  case 1:  // start row
+    im_row(b_printers);
+    Serial.write(1);
+    break;
+
+  case 2:  // write8
+    Serial.write(2);
+    uint8_t n = Serial.readBytes(b_buff, 8);
+    if (n != 8) {
+      Serial.write(0b10000000 | n);
+    }
+    b_printers.write8(b_buff);
+    Serial.write(3);
+    break;
+
+  default:
+    Serial.write(0b11111000 | command);
+    break;
+  }
+//
+//  switch (state) {
+//
+//  case WAITING:
+//    if (!Serial.available()) return;
+////    b_printers.println("go go row");
+//    state = BEGIN_ROW;
+//    return;
+//
+//  case BEGIN_ROW:
+//    im_row(b_printers);
+//    state = PRINT_ROW;
+//    return;
+//
+//  case PRINT_ROW:
+//    for (size_t i = 0; i < ROW_BYTES; i++) {
+//      for (size_t bi = 0; bi < 8; bi++) {
+//        while (Serial.available() < 8);
+//        Serial.readBytes(b_buff, 8);
+//        Serial.write(b_buff, 8);
+////        b_buff[bi] = Serial.read();
+//      }
+//      b_printers.write8(b_buff);
+//    }
+//    state = WAITING;
+//    return;
+//
+//  }
+}
+
+void im_row(Stream & s) {
+  s.write(0x12);  // DC2
+  s.write('*');   // image
+  s.write(1);     // height
+  s.write(ROW_BYTES);  // width
 }
 
